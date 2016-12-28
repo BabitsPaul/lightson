848ccc9eabe31d740d516dcd5665b90815a69b32
BabitsPaul <phi.u.934@gmail.com>
2016-12-28 18:32:52 +0100

    whatever

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,51 @@
-public class Board {
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Board
+{
+    public static final String PROPERTY_CELL = "cell";
+
+    private static final double ON_FACTOR = 0.5;
+
+    private PropertyChangeSupport propertySupport;
+
+    private int size;
+
+    private boolean[] field;
+
+    public Board(int size)
+    {
+        this.size = size;
+
+        field = new boolean[size * size];
+        propertySupport = new PropertyChangeSupport(this);
+    }
+
+    public void trigger(int x, int y) {
+        int index = x * size + y;
+
+        field[index] = !field[index];
+
+        propertySupport.fireIndexedPropertyChange("cell", index, !field[index], field[index]);
+    }
+
+    public void addPropertyChangeListener(String property, PropertyChangeListener l)
+    {
+        propertySupport.addPropertyChangeListener(property, l);
+    }
+
+    public void randomize()
+    {
+        Arrays.fill(field, false);
+
+        Random rnd = new Random();
+        for(int i = 0; i < size * size * ON_FACTOR; i++)
+        {
+            field[rnd.nextInt(size * size)] = true;
+            propertySupport.fireIndexedPropertyChange("cell", i, !field[i], field[i]);
+        }
+
+    }
 }
--- src/UI.java
+++ src/UI.java
@@ -1,10 +1,51 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.beans.IndexedPropertyChangeEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class UI {
+public class UI
+    implements PropertyChangeListener
+{
+    private static final int INIT_SIZE = 5;
+
     private Board board;
 
     private JPanel boardUI;
+
+    private JButton[] lights;
+
+    private ImageIcon onIcon, offIcon;
+
+    public UI()
+    {
+        board = new Board(INIT_SIZE);
+        board.addPropertyChangeListener(Board.PROPERTY_CELL, this);
+
+        createIcons();
+    }
+
+    private void createIcons()
+    {
+        BufferedImage onImage = new BufferedImage(128, 128, BufferedImage.TYPE_4BYTE_ABGR);
+        Graphics g = onImage.getGraphics();
+        g.setColor(Color.white);
+        g.fillRect(0, 0, 128, 128);
+        g.setColor(Color.yellow);
+        g.drawOval(32, 32, 64, 64);
+        for(double d = 0; d < Math.PI * 2; d += Math.PI / 10)
+            g.drawLine(64, 64, (int) (Math.sin(d) * 64), (int) (Math.cos(d) * 64));
+        g.dispose();
+        onIcon = new ImageIcon(onImage);
+
+        BufferedImage offImage = new BufferedImage(128, 128, BufferedImage.TYPE_4BYTE_ABGR);
+        g = offImage.getGraphics();
+        g.setColor(Color.black);
+        g.fillRect(0, 0, 128, 128);
+        g.dispose();
+        offIcon = new ImageIcon(offImage);
+    }
 
     public void createUI() {
         SwingUtilities.invokeLater(()->{
@@ -12,7 +53,7 @@ public class UI {
             header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
 
             header.add(new JLabel("Boardsize: "));
-            JSlider boardSize = new JSlider(1, 30, 5);
+            JSlider boardSize = new JSlider(1, 30, INIT_SIZE);
             boardSize.setPaintLabels(true);
             boardSize.addChangeListener(e->updateBoardSize(boardSize.getValue()));
             header.add(boardSize);
@@ -27,7 +68,7 @@ public class UI {
 
             boardUI = new JPanel();
             boardUI.setPreferredSize(new Dimension(400, 400));
-
+            updateBoardSize(INIT_SIZE);
 
             JFrame frame = new JFrame("LightsOn");
             frame.setLayout(new BorderLayout());
@@ -37,21 +78,60 @@ public class UI {
             frame.setLocationRelativeTo(null);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setVisible(true);
+
+            board.randomize();
         });
     }
 
     private void updateBoardSize(int boardSize)
     {
+        boardUI.removeAll();
+        boardUI.setLayout(new GridLayout(boardSize, boardSize));
+        lights = new JButton[boardSize * boardSize];
 
+        for(int i = 0; i < boardSize; i++)
+            for(int j = 0; j < boardSize; j++)
+            {
+                JButton light = new JButton(offIcon);
+                int x = i, y = j;
+                light.addActionListener(e->board.trigger(x, y));
+                boardUI.add(light);
+                lights[x * boardSize + y] = light;
+            }
     }
 
     private void newGame()
     {
-
+        board.randomize();
     }
 
     private void solve()
     {
 
+    }
+
+    public void won()
+    {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt)
+    {
+        switch (evt.getPropertyName())
+        {
+            case "cell":
+                int index = ((IndexedPropertyChangeEvent) evt).getIndex();
+                boolean on = (Boolean) evt.getNewValue();
+
+                SwingUtilities.invokeLater(()->{
+                    lights[index].setIcon(on ? onIcon : offIcon);
+                    lights[index].revalidate();
+                    lights[index].repaint();
+                });
+                break;
+            default:
+                throw new RuntimeException("Invalid property-name");
+        }
     }
 }
